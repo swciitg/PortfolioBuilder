b94aebfb842f89ccf1d36c35d491e9282e330881
deepak-2605 <108828387+deepak-2605@users.noreply.github.com>
2023-10-01 22:18:03 +0530

    Minor changes

--- frontend/src/components/Code.js
+++ frontend/src/components/Code.js
@@ -28,6 +28,23 @@ const Code = ({
     GitHub: ["github", "https://github.com"],
     StackOverflow: ["stack-overflow", "https://stackoverflow.com/u/"]
   };
+  const navbar= `<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                  <span class="d-block d-lg-none">${FullName}</span>
+                  <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thubmnail}" alt="${FullName}" /></span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                  </ul>
+                </div>
+              </nav>`
   return (
     <div className="Code">
       <pre className="border rounded bg-light p-3 codefile overflow-x-scroll">
@@ -72,23 +89,7 @@ const Code = ({
               </head>
               <body id="page-top">
                 <!-- Navigation-->
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-                  <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                    <span class="d-block d-lg-none">${FullName}</span>
-                    <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thubmnail}" alt="${FullName}" /></span>
-                  </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                  <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav">
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                      <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
-                    </ul>
-                  </div>
-                </nav>
+           ${navbar}
                 <!-- Page Content-->
                 <div class="container-fluid p-0">
                   <!-- About-->
--- frontend/src/components/Preview.js
+++ frontend/src/components/Preview.js
@@ -28,6 +28,23 @@ const Preview = ({
     GitHub: ["github", "https://github.com"],
     StackOverflow: ["stack-overflow", "https://stackoverflow.com/u/"]
   };
+  const navbar= `<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                  <span class="d-block d-lg-none">${FullName}</span>
+                  <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thubmnail}" alt="${FullName}" /></span>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                  <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
+                  </ul>
+                </div>
+              </nav>`
   const finalHTML = `<!DOCTYPE html>
           <html lang="en">
             <head>
@@ -69,23 +86,7 @@ const Preview = ({
             </head>
             <body id="page-top">
               <!-- Navigation-->
-              <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top">
-                  <span class="d-block d-lg-none">${FullName}</span>
-                  <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="${Thubmnail}" alt="${FullName}" /></span>
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                  <ul class="navbar-nav">
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
-                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
-                  </ul>
-                </div>
-              </nav>
+           ${navbar}
               <!-- Page Content-->
               <div class="container-fluid p-0">
                 <!-- About-->
