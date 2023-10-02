be4388f371e8165ab052030186f48b545a1a84e9
HimanshuTembhe <himanshutembhe@gmail.com>
2023-10-03 01:55:52 +0530

    Landing Page

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -1,6 +1,12 @@
 import React, { useContext } from 'react';
 import AuthContext from '../context/auth/AuthContext';
-
+import './home.css'
+import img1 from './images/one.png'
+import img2 from './images/two.png'
+import img3 from './images/three.png'
+import img4 from './images/four.png'
+import img5 from './images/swc.png'
+import img6 from './images/Microsoft.png'
 const HomePage = () => {
 
     const { userLogin } = useContext(AuthContext);
@@ -10,24 +16,37 @@ const HomePage = () => {
     }
 
     return (
-        <div
-            className="bg-cover bg-center h-screen "
-            style={{
-                backgroundImage: `url('https://static.theprint.in/wp-content/uploads/2023/07/IITG-1.jpg')`,
-            }}
-        >
-            <div className='h-full flex flex-col justify-center items-center backdrop-brightness-50'>
-                <div className='text-white font-bold text-3xl pb-6'>
-                    Portfolio Builder
-                </div>
-
-                <div className='py-2 px-7 flex border-2 border-white rounded-md text-white font-medium text-xl cursor-pointer hover:text' onClick={clickHandler}>
-                    <i class="fa-brands fa-windows text-2xl pr-3 my-auto py-auto"></i>
-                    <div className='py-auto text-xl'>Log In</div>
-                </div>
+        <div className="main">
+        <div className=" main_part">
+            <div className="header">
+                 <img src={img5}></img>
+                 <div className="heading">
+                    <p >Student web commitee - IIT Guwahati</p>
+                 </div>
+                
             </div>
-
+            <div className='bottom-part'>
+                <div className="landing_page" >
+                    <p className="head-one" >PortFolio Builder</p>
+                    <p className="head-two">Build portfolio with ease</p>
+                </div>
+                {/* <button id='btn-login' className='btn-primary'><img src={img6} /><p>Click Me</p>  </button> */}
+                <button className="button-home" onClick={clickHandler}>
+                <img className='img-btn' src={img6} />Login With Microsoft</button>
+                 
+            </div>
         </div>
+        <div className="anot">
+            <div className="part1" >
+                <img className="img_three"  src={img3} ></img>
+                <img className="img-four" src={img4} ></img>
+            </div>
+            <div className=" part2">
+               <img className="img-one" src={img1}></img>
+               <img className="img-two" src={img2}></img> 
+            </div>
+        </div>
+    </div>
     )
 }
 
