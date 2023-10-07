b3a11b1d1ea627bb9d1421db86f11e653cef32b3
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-10-07 15:46:12 +0530

    Update Form.js

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -9,7 +9,7 @@ import SkillsList from "./Form/Skills/SkillsList";
 import { SectionTitle } from "./sectionTitle/sectionTitle";
 
 const Form = ({ FormData, onChange, isExperienceEnabled, isEducationEnabled, isSkillEnabled, isInterestEnabled, isAwardsEnabled,
-toggleExperience,toggleEducation,toggleSkill,toggleInterest,toggleAward }) => {
+  isProjectEnabled,toggleProject,toggleExperience,toggleEducation,toggleSkill,toggleInterest,toggleAward }) => {
   const Desc = {
     FullName: [
       "text",
@@ -198,8 +198,19 @@ toggleExperience,toggleEducation,toggleSkill,toggleInterest,toggleAward }) => {
           {isAwardsEnabled ? 'Remove' : 'Add'} Awards
         </button>
       </div>
-      <h2 class="mb-4 mt-4">Projects</h2>
-      <ProjectList/>
+      <div>
+      {isProjectEnabled ? (
+          <>
+           <SectionTitle initialTitle="Projects" titleType="projects"/>
+           <ProjectList />
+          </>
+        ) : (
+          <p>Projects section is disabled.</p>
+        )}
+        <button onClick={toggleProject}>
+          {isProjectEnabled ? 'Remove' : 'Add'} Projects
+        </button>
+      </div>
     </div>
   );
 };
