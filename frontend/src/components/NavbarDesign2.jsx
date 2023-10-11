ea363c4ab5babe05ec9a916cc78cc79dfc8e1d09
Dhruv127 <dhruvqagarwal@gmail.com>
2023-10-11 21:33:29 +0530

    add-navbars-design-2

--- frontend/src/components/NavbarDesign2.jsx
+++ frontend/src/components/NavbarDesign2.jsx
@@ -18,7 +18,52 @@ const NavbarDesign2 = ({
   projectsTitle,
 }) => {
   return (
-   ""
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+      <a className="navbar-brand js-scroll-trigger" href="#page-top">
+        <span className="d-block d-lg-none">{FullName}</span>
+        <span className="d-none d-lg-block">
+          <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src={Thubmnail} alt={FullName} />
+        </span>
+      </a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarResponsive">
+        <ul className="navbar-nav">
+          <li className="nav-item"><a className="nav-link js-scroll-trigger" href="#about">About</a></li>
+          {isExperienceEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#experience">{experienceTitle}</a>
+            </li>
+          ) : null}
+          {isEducationEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#education">{educationTitle}</a>
+            </li>
+          ) : null}
+          {isSkillEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#skills">{skillsTitle}</a>
+            </li>
+          ) : null}
+          {isInterestEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#interests">{interestsTitle}</a>
+            </li>
+          ) : null}
+          {isAwardsEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#awards">{awardsTitle}</a>
+            </li>
+          ) : null}
+          {isProjectEnabled ? (
+            <li className="nav-item">
+              <a className="nav-link js-scroll-trigger" href="#projects">{projectsTitle}</a>
+            </li>
+          ) : null}
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- frontend/src/components/NavbarDesign4.jsx
+++ frontend/src/components/NavbarDesign4.jsx
@@ -18,7 +18,51 @@ const NavbarDesign4 = ({
   isProjectEnabled,
 }) => {
   return (
-   ""
+    <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+      <div className="container">
+        <a className="navbar-brand" href="#">{FullName}</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#about">About</a>
+            </li>
+            {isExperienceEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#experience">{experienceTitle}</a>
+              </li>
+            )}
+            {isEducationEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#education">{educationTitle}</a>
+              </li>
+            )}
+            {isSkillEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#skills">{skillsTitle}</a>
+              </li>
+            )}
+            {isInterestEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#interests">{interestsTitle}</a>
+              </li>
+            )}
+            {isAwardsEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#awards">{awardsTitle}</a>
+              </li>
+            )}
+            {isProjectEnabled && (
+              <li className="nav-item">
+                <a className="nav-link" href="#projects">{projectsTitle}</a>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
 
