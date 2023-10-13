3965f00024d6a227f1c7afd9ee3f4470080236a5
Dhruv Agarwal <92450489+Dhruv127@users.noreply.github.com>
2023-10-14 00:33:07 +0530

    Update NavbarDesign4.jsx

--- frontend/src/components/NavbarDesign4.jsx
+++ frontend/src/components/NavbarDesign4.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const NavbarDesign4 = ({
   FullName,
   Thubmnail,
@@ -23,7 +24,7 @@ const NavbarDesign4 = ({
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
         <ul className="navbar-nav ml-auto">
             <li className="nav-item">
               <a className="nav-link" href="#about">About</a>
