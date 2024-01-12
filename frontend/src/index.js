6303ba92b06b3b7ac9759b24e8530c6f8c822989
AnuragTheCoder <129210052+AnuragTheCoder@users.noreply.github.com>
2024-01-12 22:58:54 +0530

    ExperienceCard01 Made by Anurag

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
